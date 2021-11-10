4fa2af254fbae49c1a119f0d01b1e33aeca81545
inlve <inlve_jlins@outlook.com>
2021-11-10 17:01:16 +0800

    fix: bug修复
    #2

---
+++ src/fix/fix.ts
@@ -0,0 +1,7 @@
+/**
+ * this is new bug
+ */
+
+export default function fix1() {
+  console.log('🐛这是一个bug');
+}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@
  */
 import feature from '@f/feature';
 import feature1 from '@f/feature1';
+import fix1 from '@/fix/fix';
 
 feature();
 feature1();
+fix1();
